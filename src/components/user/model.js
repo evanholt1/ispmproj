e478a9ea1515656b2d85036ecb0488ae08f97b19
evan holt <adiga_knight@hotmail.com>
2020-10-27 18:50:15 +0300

    changes to user model(phone ->  email)

--- src/components/user/model.js
+++ src/components/user/model.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    patientName: {
+    name: {
         type: String
     },
-    mobileNumber: {
-        type: Number
+
+    email: {
+        type: String
+    },
+
+    password: {
+        type: String
     }
 });
 
