45ae45173e5ffef202e5fee2711163f3a25c15cb
evan holt <adiga_knight@hotmail.com>
2020-10-31 20:03:16 +0200

    clean code using beautify extension on user router file

--- src/components/user/router.js
+++ src/components/user/router.js
@@ -1,43 +1,40 @@
 const express = require('express')
-const router = express.Router({mergeParams: true})
+const router = express.Router({ mergeParams: true })
 
 const controller = require('./controller');
 
-router.post('/signup', async (req,res,next) => {
+router.post('/signup', async(req, res, next) => {
     try {
         const result = await controller.signup(req.body);
 
         const { statusCode, ...response } = result;
 
         res.status(statusCode).json(response);
-    }
-    catch(err) {
+    } catch (err) {
         next(err);
     }
 })
 
-router.post('/signin', async (req, res, next) => {
+router.post('/signin', async(req, res, next) => {
     try {
         const result = await controller.signin(req.body, req);
 
         const { statusCode, ...response } = result;
 
         res.status(statusCode).json(response);
-    }
-    catch(err) {
+    } catch (err) {
         next(err);
     }
 })
 
-router.post('/signout', async (req,res) => {
+router.post('/signout', async(req, res) => {
     try {
         const result = await controller.signout(req, res);
 
         const { statusCode, ...response } = result;
 
         res.status(statusCode).json(response);
-    }
-    catch(err) {
+    } catch (err) {
         next(err);
     }
 })
