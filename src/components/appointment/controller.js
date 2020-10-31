12fe9e354e5a93e8c15e5755afbdcec1231e6ebf
evan holt <adiga_knight@hotmail.com>
2020-10-31 20:06:58 +0200

    finalized error & response model of appointment, with cleaner code

--- src/components/appointment/controller.js
+++ src/components/appointment/controller.js
@@ -1,89 +1,95 @@
 const Appointment = require('./model');
-const { Response } = require('../../utils/response')
-const { getManySchema } = require('./validation')
-const { InputValidationError } = require('../../utils/errors')
+const validators = require('./validation');
+const { Response } = require('../../utils/response');
+const { validateInput } = require('../../utils/validation');
+const { InputValidationError } = require('../../utils/errors');
+
 
 module.exports = appointmentController = {
-    getMany: async (limit, after, service) => {
-      try {
-        await getManySchema.validateAsync({limit, after, service})
-      }
-      catch (err) { 
-        throw new InputValidationError(err.details[0].path[0], err.details[0].message)
-      }
-      
-      limit = +limit; // to make sure limit is an int not string
+    getOneById: async(id) => {
+        await validateInput(validators.getOneAppointmentSchema, id, validators.options);
 
-      if(!service) {
-        if(!limit)
-          return new Response(null, await Appointment.find().lean().exec(), false, 200);
+        const appointment = await Appointment.findById(id).lean().exec();
 
-        if(!after) 
-          return new Response(null, await Appointment.find().limit(limit).lean().exec(), false, 200);
-    
-        let appointments = await Appointment.find({ _id: { $gt:after } }).limit(limit).lean().exec();
-        
-        // if an _id was deleted, find the documents after the _id just before the one deleted, returning the same results.
-        while(!appointments)
-        appointments = Appointment.find({ _id: { $lt:after } }).sort({ _id: -1 }).limit(1).lean().exec();
-    
-        return new Response(null, appointments, false,200);
-      }
-      else {
-        if(!limit)
-        return new Response(null, await Appointment.find({ service }).lean().exec(), false, 200);
+        return new Response(null, appointment, false, 200)
+    },
 
-        if(!after) 
-          return new Response(null, await Appointment.find({ service }).limit(limit).lean().exec(), false, 200);
+    getMany: async(queryParameters) => {
+        await validateInput(validators.getManyAppointmentsSchema, queryParameters, validators.options);
 
-        let appointments = await Appointment.find({ service, _id: { $gt:after } }).limit(limit).lean().exec();
-        
-        // if an _id was deleted, find the documents after the _id just before the one deleted, returning the same results.
-        while(!appointments)
-          appointments = await Appointment.find({ service,  _id: { $lt:after } }).sort({ _id: -1 }).limit(1).lean().exec();
+        let { limit, after, service } = queryParameters;
+
+        limit = +limit; // to make sure limit is an int not string
+
+        if (!service) {
+            if (!limit)
+                return new Response(null, await Appointment.find().lean().exec(), false, 200);
+
+            if (!after)
+                return new Response(null, await Appointment.find().limit(limit).lean().exec(), false, 200);
+
+            let appointments = await Appointment.find({ _id: { $gt: after } }).limit(limit).lean().exec();
+
+            // if an _id was deleted, find the documents after the _id just before the one deleted, returning the same results.
+            while (!appointments)
+                appointments = Appointment.find({ _id: { $lt: after } }).sort({ _id: -1 }).limit(1).lean().exec();
+
+            return new Response(null, appointments, false, 200);
+        } else {
+            if (!limit)
+                return new Response(null, await Appointment.find({ service }).lean().exec(), false, 200);
+
+            if (!after)
+                return new Response(null, await Appointment.find({ service }).limit(limit).lean().exec(), false, 200);
+
+            let appointments = await Appointment.find({ service, _id: { $gt: after } }).limit(limit).lean().exec();
+
+            // if an _id was deleted, find the documents after the _id just before the one deleted, returning the same results.
+            while (!appointments)
+                appointments = await Appointment.find({ service, _id: { $lt: after } }).sort({ _id: -1 }).limit(1).lean().exec();
+
+            return new Response(null, appointments, false, 200);
+        }
+    },
+
+    addMany: async(appointmentsInputs) => {
+        await validateInput(validators.addAppointmentsSchema, appointmentsInputs, validators.options);
+
+        let appointments = [];
+
+        let appointmentsAdded = await Appointment.create(appointmentsInputs); // create accepts an array, and deals with it properly
+
+        appointmentsAdded.forEach(appointment => {
+            appointments.push(appointment.toObject({ getters: true }));
+        });
 
         return new Response(null, appointments, false, 200);
-      }
     },
 
-    getOneById: async(id) => {
-      const appointment = await Appointment.findById(id).lean().exec()
-      
-      return new Response(null, appointment, false, 200)
-    },
-  
-    addMany: async(appointmentsInputs) => {
-      let appointments = []
-
-      let appointmentsAdded = await Appointment.create(appointmentsInputs); // create accepts an array, and deals with it properly
-    
-      appointmentsAdded.forEach(appointment => {
-        appointments.push(appointment.toObject( { getters:true } ))
-      });
-
-      return new Response(null, appointments, false, 200)
-    },
-  
     editMany: async(appointmentsInputs) => {
-      let updatedAppointments = [];
+        await validateInput(validators.editAppointmentsSchema, appointmentsInputs, validators.options);
 
-      // foreach faces issue with async/await
-      for (const appointmentInput of appointmentsInputs) {
-        let updatedAppointment = await Appointment.findByIdAndUpdate(appointmentInput._id, appointmentInput, {new: true} ).exec();
-        updatedAppointments.push(updatedAppointment);
-      }
-      return new Response(null, updatedAppointments, false, 200);
+        let updatedAppointments = [];
+
+        // foreach faces issue with async/await
+        for (const appointmentInput of appointmentsInputs) {
+            let updatedAppointment = await Appointment.findByIdAndUpdate(appointmentInput._id, appointmentInput, { new: true }).exec();
+            updatedAppointments.push(updatedAppointment);
+        }
+        return new Response("Successfully updated selected appointment", updatedAppointments, false, 200);
     },
-  
-    removeMany: async(appointmentsIds) => {
-      const appointmentsToDelete = await Appointment.find({ _id: { $in:appointmentsIds } }).exec();
 
-      appointmentsToDelete.forEach(appointment => {
-        appointment.remove();
-  
-        appointment = appointment._doc;
-      });
-  
-      return new Response(null, appointmentsToDelete, false, 200);
+    removeMany: async(appointmentsIds) => {
+        await validateInput(validators.removeAppointmentsSchema, appointmentsIds, validators.options);
+
+        const appointmentsToDelete = await Appointment.find({ _id: { $in: appointmentsIds } }).exec();
+
+        appointmentsToDelete.forEach(appointment => {
+            appointment.remove();
+
+            appointment = appointment._doc;
+        });
+
+        return new Response(null, appointmentsToDelete, false, 200);
     }
 }
--- src/components/appointment/routes.js
+++ src/components/appointment/routes.js
@@ -1,34 +1,53 @@
 const express = require('express');
-const router = express.Router({mergeParams: true})
+const router = express.Router({ mergeParams: true })
 
 const controller = require('./controller');
 const { validateSession } = require('../../utils/validateSession');
 
-router.get('/', async(req,res) => {
-    const result = await controller.getMany(req.query.limit, req.query.after, req.query.service);
 
-    res.send(result);
+router.get('/', async(req, res, next) => {
+    try {
+        const result = await controller.getMany(req.query);
+
+        const { statusCode, ...response } = result;
+
+        res.status(statusCode).json(response);
+    } catch (err) {
+        next(err);
+    }
 })
 
-router.get('/:id', async(req,res) => {
-    const result = await controller.getOneById(req.params.id);
+router.get('/:id', async(req, res, next) => {
+    try {
+        const result = await controller.getOneById(req.params.id);
 
-    res.send(result);
+        const { statusCode, ...response } = result;
+
+        res.status(statusCode).json(response);
+    } catch (err) {
+        next(err);
+    }
 })
 
-router.post('/', validateSession, async(req,res) => {
-    const result = await controller.addMany(req.body);
+router.post('/', validateSession, async(req, res, next) => {
+    try {
+        const result = await controller.addMany(req.body);
 
-    res.send(result);
+        const { statusCode, ...response } = result;
+
+        res.status(statusCode).json(response);
+    } catch (err) {
+        next(err);
+    }
 })
 
-router.patch('/', validateSession, async(req,res) => {
+router.patch('/', validateSession, async(req, res, next) => {
     const result = await controller.editMany(req.body);
 
     res.send(result);
 })
 
-router.delete('/', validateSession, async(req,res) => {
+router.delete('/', validateSession, async(req, res, next) => {
     const result = await controller.removeMany(req.body);
 
     res.send(result);
