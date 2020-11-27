7bd7a866d0d8a9ebd9c876aa3b4a60b95f98ff9a
evan holt <adiga_knight@hotmail.com>
2020-11-27 20:14:22 +0200

    removed hospital router

--- src/utils/router.js
+++ src/utils/router.js
@@ -2,12 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const userRouter = require('../components/user/router');
-const hospitalRouter = require('../components/hospital/routes');
 const appointmentRouter = require('../components/appointment/routes');
 const errorRouter = require('./errorRouter')
 
 router.use('/user', userRouter);
-router.use('/hospital', hospitalRouter);
 router.use('/appointment', appointmentRouter);
 router.use(errorRouter)
 
