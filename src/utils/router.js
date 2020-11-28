52e12547a9f3671823ff8ee673f13dfbdc720215
evan holt <adiga_knight@hotmail.com>
2020-11-28 10:25:14 +0200

    started integration with views with ejs and home route

--- src/utils/router.js
+++ src/utils/router.js
@@ -5,8 +5,16 @@ const userRouter = require('../components/user/router');
 const appointmentRouter = require('../components/appointment/routes');
 const errorRouter = require('./errorRouter')
 
-router.use('/user', userRouter);
-router.use('/appointment', appointmentRouter);
+router.get('/', (req, res, next) => {
+    res.render('Home')
+})
+
+
+router.use('/api/user', userRouter);
+router.use('/api/appointment', appointmentRouter);
+
+
+
 router.use(errorRouter)
 
 module.exports = router;
