dc7ad47b5a565f37b99aeca730429316dfde412b
evan holt <adiga_knight@hotmail.com>
2020-12-07 10:55:23 +0200

    updated main router 2

--- src/utils/router.js
+++ src/utils/router.js
@@ -6,11 +6,6 @@ const appointmentRouter = require('../components/appointment/routes');
 const employeeRouter = require('../components/employee/routes');
 const errorRouter = require('./errorRouter')
 
-<<<<<<< HEAD
-router.use('/user', userRouter);
-router.use('/appointment', appointmentRouter);
-router.use('/employee', employeeRouter);
-=======
 router.get('/', (req, res, next) => {
     res.render('Home')
 })
@@ -18,10 +13,9 @@ router.get('/', (req, res, next) => {
 
 router.use('/api/user', userRouter);
 router.use('/api/appointment', appointmentRouter);
+router.use('/api/employee', employeeRouter);
 
 
-
->>>>>>> 695286ff4f260e189e8a57e7cdd46a4b70c52228
 router.use(errorRouter)
 
 module.exports = router;
