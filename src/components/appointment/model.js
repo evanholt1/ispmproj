a65d0c41a418e86d9b401e1e783d94880aeb2363
evan holt <adiga_knight@hotmail.com>
2020-12-27 17:54:06 +0200

    more appointment.ejs bugfix

--- src/components/appointment/model.js
+++ src/components/appointment/model.js
@@ -1,95 +1,92 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const { serviceNames, serviceStates } = require('../../utils/services');
-const { pointSchema } = require('../../utils/pointSchema')
-const { DateTime } = require('luxon');
+const { serviceNames, serviceStates } = require("../../utils/services");
+const { pointSchema } = require("../../utils/pointSchema");
+const { DateTime } = require("luxon");
 
-const AppointmentSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    service: {
-        type: String,
-        enum: serviceNames
-    },
-    location: {
-        type: pointSchema
-    },
-    date: {
-        type: Date,
-        get: toLocalDate
+const AppointmentSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        service: {
+            type: String,
+            enum: serviceNames,
+        },
+        location: {
+            type: pointSchema,
+        },
+        date: {
+            type: Date,
+            get: toLocalDate,
             // format: UTC date
             // no setter as formatting is done in 'addMany' before new Appointment() is called. (.create() to be precise)
-
+        },
+        state: {
+            // blood collection,hemodialysis, medicine delivery, home medical services have different states.
+            // business logic code will control this
+            type: Number,
+            set: updateState,
+            get: getStateText,
+        },
+        allocatedStaff: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Employee",
+            },
+        ],
+        isFinished: {
+            type: Boolean,
+            default: false,
+        },
     },
-    state: { // blood collection,hemodialysis, medicine delivery, home medical services have different states. 
-        // business logic code will control this
-        type: Number,
-        set: updateState,
-        get: getStateText
-    },
-    allocatedStaff: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Employee'
-    }],
-    isFinished: {
-        type: Boolean,
-        default: false
+    {
+        toObject: { getters: true, setters: true },
+        toJSON: { getters: true, setters: true },
+        runSettersOnQuery: true,
     }
-}, {
-    toObject: { getters: true, setters: true },
-    toJSON: { getters: true, setters: true },
-    runSettersOnQuery: true
-});
+);
 
-var PreAutoPopulate = function(next) {
-    this.populate({ path: 'allocatedStaff', select: 'name' })
-        .populate({ path: 'user', select: 'name' });
+var PreAutoPopulate = function (next) {
+    this.populate({ path: "allocatedStaff", select: "name" }).populate({ path: "user", select: "name" });
     next();
 };
 
-AppointmentSchema.
-pre('findOne', PreAutoPopulate).
-pre('find', PreAutoPopulate);
+AppointmentSchema.pre("findOne", PreAutoPopulate).pre("find", PreAutoPopulate);
 
-const advanceState = function(currentState, service, isAppointmentFinished) {
+const advanceState = function (currentState, service, isAppointmentFinished) {
     switch (service) {
         case "Hemodialysis Request":
-            if (currentState < 3)
-                currentState += 1;
-            if (currentState == 3) // appointment state is now "fufilled"
+            if (currentState < 3) currentState += 1;
+            if (currentState == 3)
+                // appointment state is now "fufilled"
                 isAppointmentFinished = true; // frees employees and isFinished becomes true
 
             break;
         default:
-            if (currentState < 5)
-                currentState += 1;
-            if (currentState == 5)
-                isAppointmentFinished = true;
+            if (currentState < 5) currentState += 1;
+            if (currentState == 5) isAppointmentFinished = true;
             break;
     }
     return {
-        'currentState': currentState,
-        'isFinished': isAppointmentFinished
+        currentState: currentState,
+        isFinished: isAppointmentFinished,
     };
 };
 
-const revertState = function(currentState, service) {
+const revertState = function (currentState, service) {
     switch (service) {
         case "Hemodialysis Request":
-            if (currentState > -1 && currentState != 3)
-                currentState -= 1;
+            if (currentState > -1 && currentState != 3) currentState -= 1;
             break;
         default:
-            if (currentState > -1 && currentState != 5)
-                currentState -= 1;
+            if (currentState > -1 && currentState != 5) currentState -= 1;
             break;
     }
     return currentState;
 };
-
 
 function toLocalDate(date) {
     let appointmentDate = DateTime.fromJSDate(date, { zone: "Asia/Amman" }).toISO();
@@ -113,7 +110,7 @@ function updateState(StateInput) {
 
         return newState;
     }
-    if (!this.state) return 0
+    if (!this.state || StateInput == 0) return 0;
     else return this.state;
     // if (newStateIndex > this.state) {
     //     const result = advanceState(this.service, this.state, newStateIndex, this.isFinished);
@@ -158,4 +155,4 @@ function getStateIndex(state, service) {
             return serviceStates.homeMedicalServicesStates.indexOf(state);
     }
 }
-module.exports = mongoose.model('Appointment', AppointmentSchema, "appointments");
+module.exports = mongoose.model("Appointment", AppointmentSchema, "appointments");
